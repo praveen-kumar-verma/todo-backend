6aef1928b27c641ccac1be25a7e411cfdf314633
praveen-kumar-verma <vpraveen760@gmail.com>
2024-08-01 00:31:29 +0530

    ready for prod

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express()
 const signinRouter = require("./routes/signin")
 const signupRouter = require("./routes/signup")
 const taskRouter = require("./routes/tasks")
+const verifyTokenRouter = require("./routes/verify-token")
+const userDetailRouter = require("./routes/user-detail")
 
+
+var cors = require('cors')
 const mongoose = require("mongoose");
 
 
 
 app.use(express.json());
+app.use(cors())
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -17,6 +22,9 @@ mongoose
 app.use("/",signinRouter)
 app.use("/signup", signupRouter)
 app.use("/task", taskRouter)
+app.use("/verify-token",verifyTokenRouter )
+app.use("/user-detail",userDetailRouter )
+
 
 
 const PORT = process.env.PORT || 5000;
